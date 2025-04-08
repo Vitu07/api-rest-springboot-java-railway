b955f078af253f9074b829f1c303d72cf36aa822
Vitu07 <vitormorais20092004@gmail.com>
2025-04-08 18:15:31 -0300

    Criando perfil de prd

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Apllication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Apllication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
